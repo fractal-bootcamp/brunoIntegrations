31fcd6a94596e135da517e8a7a2d309c843e3f20
brunoLloret <163921771+brunoLloret@users.noreply.github.com>
2024-07-08 18:47:57 -0400

    Frontend (Homepage+Auth)

--- Project/frontend/src/main.tsx
+++ Project/frontend/src/main.tsx
@@ -3,6 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './layouts/root-layout.tsx'
+import ProtectedRoute from '../src/components/ProtectedRoute.tsx'
+import Home from "./components/Home"
+import SignIn from './routes/sign-in.tsx'
+import SignUp from './routes/sign-up.tsx'
+//import MailingListManager
+//import ToolkitEditor
+
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/sign-in/*", element: <SignIn /> },
+      { path: "/sign-up/*", element: <SignUp /> },
+      {
+        path: "/Mailing-list-manager",
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/Editor",
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -14,7 +52,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <RouterProvider router={router} />
     </ClerkProvider>
   </React.StrictMode>,
 )
